efa131c77c783e531610dd7d589a2b45449ef347
Will Rathlou <willrathlou@hotmail.com>
2017-11-22 20:04:35 +0000

    Meal Planner V1
    Code for healthy eating windows form app

---
+++ Meal Planner [Incomplete]/WindowsFormsApplication3/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Gray;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(101, 497);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(216, 54);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Validate";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(50, 241);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(305, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(79, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(266, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Please fill in the following fields\r\n";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(46, 178);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(313, 48);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Please enter your desired maximum\r\n calorie intake for this meal";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.White;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Breakfast",
+            "Lunch",
+            "Dinner"});
+            this.comboBox1.Location = new System.Drawing.Point(50, 114);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(305, 21);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(181, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 24);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Meal";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 324);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(384, 24);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Shall I open the recipe in your web browser?";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Yes please",
+            "No thankyou"});
+            this.comboBox2.Location = new System.Drawing.Point(50, 362);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(305, 21);
+            this.comboBox2.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(438, 563);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Calorie Based Meal Selector";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox2;
+    }
+}
+
---
+++ Meal Planner [Incomplete]/WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        public Random rand = new Random();
+        int calories;
+        int holder;
+        string parses;
+        //breakfast
+        string[] breakfast =
+        {
+        "Yogurt & Honey Fruit Cups" + "\n" + "\n"+ "4-1/2 cups cut-up fresh fruit (pears, apples, bananas, grapes, etc.)" + "\n" + "3/4 cup (6 ounces) mandarin orange, vanilla or lemon yogurt" + "1 tablespoon honey" + "\n" + "1/2 teaspoon grated orange peel" + "\n" + "1/4 teaspoon almond extract" + "\n" + "\n" +"Total Calories : 97 Kcal",
+        "Eggs Benedict" + "\n" +"\n" + "1 slice white bread" + "\n" + "1 egg" + "\n" + "50ml skimmed milk" + "\n" + "60g asparagus" + "\n" + "\n" + "Total Calories : 150Kcal",
+        "Strawberry + Banana Chocolate Chip Oat Muffins" + "\n" + "\n" + "1 1/4 cup whole wheat pastry flour" + "\n" + "1 cup rolled oats" + "\n" + "3/4 teaspoon baking soda" + "\n" + "1/2 teaspoon baking powder" + "\n" + "1/4 teaspoon salt" + "\n" + "1 heaping cup mashed bananas (about 2 to 3 large very ripe bananas)" + "\n" + "1 tablespoon olive oil" + "\n" + "1 tablespoon honey or agave nectar" + "\n" + "1 teaspoon vanilla" + "\n" + "1 egg" + "\n" + "1 egg white" + "\n" + "1/3 cup nonfat plain greek yogurt" + "\n" + "1/2 cup unsweetened vanilla almond milk" + "\n" + "1/3 cup mini chocolate chips" + "\n" + "2/3 cup diced strawberries" + "\n" + "12 thin slices of strawberries (about 3-4 strawberries) for topping, if desired" + "\n" + "\n" +"Total Calories : 150 Kcal per muffin",
+        };
+        string[] breakfastlink =
+        {
+        "https://goo.gl/4Aryoo",
+        "https://goo.gl/BhDdwR",
+        "https://goo.gl/f9NVQ7"
+        };
+        // lunch
+        string[] lunch = { };
+        //dinner
+        string[] dinner = { };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            parses = textBox1.Text;
+            if (!textBox1.Text.All(char.IsDigit) || string.IsNullOrWhiteSpace(parses))
+            {
+                MessageBox.Show("Please enter a valid value");
+            }       
+            else
+            {
+                calories = int.Parse(textBox1.Text);
+                int selector = howmany(calories, holder);
+                if (comboBox2.Text == "Yes please")
+                {
+                    if (comboBox1.Text == "Breakfast")
+                    {
+                    System.Diagnostics.Process.Start(breakfastlink[selector]);
+                    }
+                    if (comboBox1.Text == "Lunch")
+                    {
+                        MessageBox.Show(lunch[selector]);
+                    }
+                    if (comboBox1.Text == "Dinner")
+                    {
+                        MessageBox.Show(dinner[selector]);
+                    }
+                    
+                    }
+                else if (comboBox2.Text == "No thankyou")
+                {
+                    if (comboBox1.Text == "Breakfast")
+                    {
+                        MessageBox.Show(breakfast[selector]);
+                    }
+                    if (comboBox1.Text == "Lunch")
+                    {
+                        MessageBox.Show(lunch[selector]);
+                    }
+                    if (comboBox1.Text == "Dinner")
+                    {
+                        MessageBox.Show(dinner[selector]);
+                    }
+                }
+                
+
+            }
+
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+        private int howmany(int transfer, int builder)
+        {
+           if (151> transfer)
+            {
+                builder = rand.Next(0,3);
+            }
+           else if (300 > transfer)
+            {
+                builder = rand.Next(0, 40);
+            }
+           else if (450 > transfer)
+            {
+                builder = rand.Next(0, 60);
+            }
+           else if (transfer > 451)
+            {
+                builder = rand.Next(0, 80);
+            }
+            return builder;
+        }
+    }
+}
